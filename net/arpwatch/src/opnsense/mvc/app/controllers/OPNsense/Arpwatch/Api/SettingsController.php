9ac32cc63c2a208050e05910dbb186e7ef0ea1f1
Cameron Cooper <coopercc06@gmail.com>
2024-11-22 02:20:15 -0600

    pls fucking work

--- net/arpwatch/src/opnsense/mvc/app/controllers/OPNsense/Arpwatch/Api/SettingsController.php
+++ net/arpwatch/src/opnsense/mvc/app/controllers/OPNsense/Arpwatch/Api/SettingsController.php
@@ -4,6 +4,6 @@ namespace OPNsense\Arpwatch\Api;
 use \OPNsense\Base\ApiMutableModelControllerBase;
 class SettingsController extends ApiMutableModelControllerBase
 {
-  public static $internalModelClass = 'OPNsense\Arpwatch\Arpwatch';
-  public static $internalModelName = 'arpwatch';
+  protected static $internalModelClass = 'OPNsense\Arpwatch\Arpwatch';
+  protected static $internalModelName = 'arpwatch';
 }
