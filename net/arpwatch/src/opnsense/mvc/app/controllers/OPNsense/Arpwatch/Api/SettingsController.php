dd16a8c60ef5b67927c5277d1909e85e7c67af0d
Cameron Cooper <coopercc06@gmail.com>
2024-11-22 02:24:33 -0600

    for some reason it doesnt register

--- net/arpwatch/src/opnsense/mvc/app/controllers/OPNsense/Arpwatch/Api/SettingsController.php
+++ net/arpwatch/src/opnsense/mvc/app/controllers/OPNsense/Arpwatch/Api/SettingsController.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace OPNsense\Arpwatch\Api;
 
-use \OPNsense\Base\ApiMutableModelControllerBase;
+use OPNsense\Base\ApiMutableModelControllerBase;
+
 class SettingsController extends ApiMutableModelControllerBase
 {
-  protected static $internalModelClass = 'OPNsense\Arpwatch\Arpwatch';
-  protected static $internalModelName = 'arpwatch';
+    protected static $internalModelClass = 'OPNsense\Arpwatch\Arpwatch';
+    protected static $internalModelName = 'arpwatch';
 }
