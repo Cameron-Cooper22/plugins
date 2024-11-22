1b22806e7d02b6d7c8592b2eb032f8c5202cedad
Cameron Cooper <coopercc06@gmail.com>
2024-11-22 03:35:10 -0600

    please

---
+++ net/arpmonitor/src/opnsense/mvc/app/controllers/OPNsense/Arpmonitor/Api/SettingsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OPNsense\Arpmonitor\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+
+class SettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelClass = '\OPNsense\Arpmonitor\Arpmonitor';
+    protected static $internalModelName = 'arpmonitor';
+}
--- net/arpmonitor/src/opnsense/mvc/app/controllers/OPNsense/Arpmonitor/IndexController.php
+++ net/arpmonitor/src/opnsense/mvc/app/controllers/OPNsense/Arpmonitor/IndexController.php
@@ -6,7 +6,7 @@ class GeneralController extends \OPNsense\Base\IndexController
 {
     public function indexAction(): void
     {
-      $this -> view -> pick('OPNsense/Arpwatch/general');
+      $this -> view -> pick('OPNsense/Arpwatch/index');
       $this -> view -> generalForm = $this -> getForm("general");
     }
 }
